e41e38b5f527d481148f01285d72043b40ae728d
Peng Xue Vang <pxvang33@gmail.com>
2019-01-04 08:25:08 -0600

    setup node express done

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(express.static('server/public'));
+
+const PORT = 5003;
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(PORT, () =>{
+    console.log(`listening on port ${PORT}`);
+})
